be9ae14815710fbf21a872bb60cb42558df59cf7
Camila <camidv14@gmail.com>
2018-02-27 22:32:50 -0500

    Parcial Monte Fuji

---
+++ MariaCamilaDaza-MountFuji/Assets/Script/movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public float Dx;
+	public float Dy;
+	public float Dz;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (Dx, Dy, Dz);
+	}
+}
